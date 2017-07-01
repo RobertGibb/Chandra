26a15039d6dbfe65f7eeaa73733150e273c076cb
Robert Gibb <rgibb11@gmail.com>
2017-07-01 00:34:05 -0400

    autosuggest updates

--- src/components/AutoSuggest.js
+++ src/components/AutoSuggest.js
@@ -7,22 +7,22 @@ export default class AutoSuggest extends React.Component {
 
     constructor(){
         super();
-        this.state = {isSearching: false};
+        this.state = {  isSearching: false,
+                        prevSearches: [{
+                            name: "search1",
+                        }, {
+                            name: "search2"}]};
+
+        this.search = this.search.bind(this);
 
     }
 
-    prevSearches = () => {
-        var prevSearches = [{
-            name: "search1",
-        }, {
-            name: "search2",
-        }];
-        return prevSearches;
-    }
-
-    search = () => {
+    search(){
+        console.log(this.searchInput.value);
+        var newPrevSearches = this.state.prevSearches.slice();
+        newPrevSearches.push({name: this.searchInput.value});
         this.setState({isSearching: true});
-        console.log("inside search")
+        this.setState({prevSearches: newPrevSearches})
         // This probably where you would have an `ajax` call
         setTimeout(() => {
             // Completed of async action, set loading state back
@@ -39,7 +39,11 @@ export default class AutoSuggest extends React.Component {
             <Grid>
                 <Row>
                     <Col md={4}>
-                        <FormControl />
+                        <FormControl
+                            componentClass="input"
+                            inputRef={(ref) => {this.searchInput = ref}}
+
+                        />
                     </Col>
                     <Col md={1}>
                         <Button
@@ -54,7 +58,7 @@ export default class AutoSuggest extends React.Component {
                 </Row>
                 <Row>
                     <Col md={4}>
-                        <BootstrapTable tableHeaderClass="col-hidden" data={this.prevSearches()} striped={true} hover={true}>
+                        <BootstrapTable tableHeaderClass="col-hidden" data={this.state.prevSearches} striped={true} hover={true}>
                             <TableHeaderColumn className="col-hidden" dataField="name" isKey={true}>Data Source</TableHeaderColumn>
                         </BootstrapTable>
                     </Col>
